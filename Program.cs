f9c0e5cf938f9dea8851e9f05acbaf5cdf0cadba
Илья <machibosanofgod@gmail.com>
2023-02-13 16:06:29 +0300

    задал массив

--- Program.cs
+++ Program.cs
@@ -10,3 +10,11 @@
 // ["1234", "1567", "-2", "computer science"] => ["-2"]
 
 // ["Russia", "Denmark", "Kazan"] => []
+
+
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    
+}
